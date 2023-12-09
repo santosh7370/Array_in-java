37702aee0148820aecf9efe8167abab36d08a50c
Santosh Singhaniya <116973860+santosh7370@users.noreply.github.com>
2023-12-10 00:04:30 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,18 @@
+//using syntax 
+// dataType array_name[]=new dateType[Size];
+public class Array2 
+{
+    public static void main(String[] args) {
+        int a[]=new int[5];
+        a[0]=12;
+        a[1]=13;
+        a[2]=15;
+        a[3]=14;
+        a[4]=16;
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+
+    }
+}
---
+++ Array3.java
@@ -0,0 +1,18 @@
+/*
+//printing array using for-each loop  
+ for(data_type variable:array)
+ {  
+ body of the loop  
+ }  
+ */
+public class Array3 
+{
+    public static void main(String args[])
+    {  
+        int num[]={33,43,22,134,542,54,654};  
+        for(int i:num)
+        { 
+        System.out.println(i); 
+        } 
+    } 
+}
