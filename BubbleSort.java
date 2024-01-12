3779869b8c9ec2343f39132b74778676b0983f47
Santosh Singhaniya <116973860+santosh7370@users.noreply.github.com>
2024-01-12 21:49:44 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class BubbleSort 
+{
+    public static void sort(int arr[])
+    {
+        for(int turn=0; turn<arr.length-1; turn++)
+        {
+            for(int j=0; j<arr.length-1-turn; j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void printArr(int arr[])
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) 
+    {
+        int arr[]={5,3,2,1,4};
+        sort(arr);
+        System.out.println("Bobble Sorted Array");
+        printArr(arr);
+    }
+}
